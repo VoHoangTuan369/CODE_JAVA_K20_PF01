abe3e8893230ed601390f62735ff6a02fb22176a
VoHoangTuan369 <tuanvuivb@gmailcom>
2021-05-10 11:04:44 +0700

    test thread

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,26 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Function 1");
+        new Thread(()->{
+            for (int i = 0; i < 10; i++) {
+                System.out.print(" a ");
+                try{
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+        new Thread(()->{
+            for (int i = 0; i < 10; i++) {
+                System.out.print(" b ");
+                try{
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+        System.out.print(" end main ");
     }
 }
